91c09fe3df3ac9e6f2efbeddacd65208b3876189
guiafredinho <guiafredinhogarapua@gmail.com>
2025-09-18 22:36:10 -0300

    Create use.js

---
+++ use.js
@@ -0,0 +1,48 @@
+// modelos/User.js
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+// Estrutura do usuário
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Antes de salvar, criptografa a senha
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Método para comparar senhas no login
+UserSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
